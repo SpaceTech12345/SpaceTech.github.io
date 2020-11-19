6f2116a50b33fee61bd3358ac99f1fdb1b3da30c
SpaceTech <n181432@gmail.com>
2020-11-19 17:16:33 +0530

    created for geek a hertz
    this is created by navneet and akshat

---
+++ main.js
@@ -0,0 +1,28 @@
+function menubtn(){
+var slide= document.getElementById('slide-out');
+if(slide.style.display === "inline-block"){
+slide.style.display="none";
+}else{
+slide.style.display="inline-block";
+search.style.display="none";
+}
+}
+searchbtn=()=>{
+var search= document.getElementById('bar');
+if(search.style.display === "block"){
+search.style.display="none";
+}else{
+search.style.display="block";
+}
+}
+const searchIcon = document.getElementById("search-icon");
+const searchBox = document.getElementById("searchbox");
+searchIcon.addEventListener('click', function () {
+if (searchBox.style.top == '72px') {
+searchBox.style.top = '24px';
+searchBox.style.pointerEvents = 'none';
+} else {
+searchBox.style.top = '72px';
+searchBox.style.pointerEvents = 'auto';
+}
+});
